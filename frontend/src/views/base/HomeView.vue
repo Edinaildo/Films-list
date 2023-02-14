99c3c9ce7a7ab0242477ffdb0ab82c22e28bb060
Edinaildo <edinaildopereira2013@gmail.com>
2023-02-14 20:09:36 -0300

    versão modificada

---
+++ frontend/src/views/base/HomeView.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <v-img contain height="240" src="@/assets/film.png" />
+      <blockquote class="blockquote text-h5">
+        &#8220;A sua lista de filmes preferidos em um só lugar!&#8221;
+        <footer>
+          <small>
+            <em>&mdash;FILMS LIST &mdash;</em>
+          </small>
+        </footer>
+      </blockquote>
+
+
+      <div class="py-6" />
+
+      <v-row class="d-flex align-center justify-center">
+        <v-col cols="auto">
+          <v-btn
+            color="dark-blue"
+            :to="{ name: 'base-getstarted' }"
+            min-width="228"
+            rel="noopener noreferrer"
+            size="x-large"
+            variant="flat"
+            class="my-4">
+            <v-icon icon="mdi-speedometer" size="large" start />
+            SOBRE
+          </v-btn>
+          <v-btn
+            v-if="!loggedUser"
+            color="blue"
+            min-width="228"
+            rel="noopener noreferrer"
+            size="x-large"
+            variant="flat"
+            :to="{ name: 'accounts-login' }"
+            class="my-4">
+            <v-icon icon="mdi-account-arrow-right-outline" size="large" start />
+            Login
+          </v-btn>
+          <v-btn
+            v-else
+            color="blue"
+            min-width="228"
+            rel="noopener noreferrer"
+            size="x-large"
+            variant="flat"
+            :to="{ name: 'accounts-logout' }">
+            <v-icon icon="mdi-account-arrow-right-outline" size="large" start />
+            Logout
+          </v-btn>
+          <v-btn
+            v-if="loggedUser"
+            color="blue"
+            min-width="228"
+            rel="noopener noreferrer"
+            size="x-large"
+            variant="flat"
+            :to="{ name: 'tasks-list' }"
+            class="my-4">
+            <v-icon icon="mdi-folder-star-multiple" size="large" start />
+            tarefas
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script>
+import { mapState } from "pinia"
+import { useAccountsStore } from "@/stores/accountsStore"
+
+export default {
+  computed: {
+    ...mapState(useAccountsStore, ["loggedUser"]),
+  },
+}
+</script>
